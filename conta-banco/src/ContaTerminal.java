ecb713b2587979eaed2c72df1f5c845f39d06fea
cicerojr10 <cicerojr10@gmail.com>
2024-05-15 23:27:28 -0300

    projeto inicial finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        // Scanner em um bloco try-with-resources
+        try (Scanner scanner = new Scanner(System.in)) { 
 
         // Declaração de variáveis para armazenar dados da conta
         int numeroConta;
-        String agencia;
+        String numeroAgencia;
         String nomeCliente;
         double saldo;
 
         // Coleta de dados do usuário
-        System.out.println("** Seja bem-vindo ao Banco XYZ! **");
         System.out.println("** Criação de Conta Bancária **");
 
         System.out.print("Por favor, digite o número da agência: ");
-        agencia = scanner.nextLine();
+        numeroAgencia = scanner.nextLine();
 
         System.out.print("Por favor, digite o número da conta: ");
         numeroConta = scanner.nextInt();
@@ -27,7 +27,7 @@ public class ContaTerminal {
 
         String saldoString; // Variável para armazenar a entrada do saldo como string
         do {
-            System.out.print("Por favor, digite o saldo inicial (ex: 123,45): ");
+            System.out.print("Por favor, digite o saldo inicial (ex: 123.45): ");
             saldoString = scanner.next();
         } while (!saldoString.matches("\\d+(\\.\\d+)?")); // Validação do formato decimal
 
@@ -36,11 +36,10 @@ public class ContaTerminal {
         // Apresentação dos dados da conta
         System.out.println("\n** Conta Criada com Sucesso! **");
         System.out.println("---------------------------------");
-        System.out.println("Olá " + nomeCliente + ",");
-        System.out.println("Sua conta (" + numeroConta + ") foi criada na agência " + agencia + ".");
-        System.out.println("Seu saldo inicial é de R$ " + saldo + ".");
-        System.out.println("---------------------------------");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar sua conta em nosso banco, sua agencia é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo "
+        + saldo + " já está disponível para saque.");
 
-        System.out.println("\n** Obrigado por criar uma conta no Banco XYZ! **");
+        }
     }
+
 }
